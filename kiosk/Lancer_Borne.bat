db4ee8767148edbf127675dd22583235289bc8a7
Saïf <saifeddinemiah@gmail.com>
2026-02-20 21:43:52 +0100

    Ajout script de lancement borne avec impression silencieuse

--- kiosk/Lancer_Borne.bat
+++ kiosk/Lancer_Borne.bat
@@ -1,3 +1,13 @@
 @echo off
-echo Demarrage de la Borne Chickenzz...
-start chrome --kiosk --kiosk-printing "%~dp0index.html"
+REM ============================================
+REM   LANCEMENT BORNE CHICKENZZ
+REM   Ce script lance Chrome en mode borne
+REM   avec impression automatique (sans popup)
+REM ============================================
+
+REM Fermer Chrome s'il est deja ouvert
+taskkill /IM chrome.exe /F >nul 2>&1
+timeout /t 2 /nobreak >nul
+
+REM Lancer Chrome en mode kiosk avec impression silencieuse
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --kiosk-printing --disable-pinch --overscroll-history-navigation=0 --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --check-for-update-interval=31536000 "%~dp0index.html"
